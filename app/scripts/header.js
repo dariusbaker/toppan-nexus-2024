f45a846699981e23cfa4b8c845d611943be15443
Kencana Kesuma <kesuma.kencana@yahoo.com>
2024-04-24 19:15:54 +0800

    GH-3 add(header): add mobile nav drawer + logic

--- app/scripts/header.js
+++ app/scripts/header.js
@@ -1,20 +1,43 @@
 const CLASSES = {
-  DRAWER_OPEN: 'drawer-open',
-  SCROLLING: 'scrolling',
+  DRAWER_OPEN: 'header-drawer-open',
+  SCROLLING: 'header-scrolling',
   SUBMENU_TOGGLER: 'header__nav__submenu-toggler',
+};
+
+const IDS = {
+  NAV_TOGGLER: 'nav-menu-toggle',
+  HEADER: 'toppan-header',
 };
 
 export default class Header {
   constructor() {
-    this.header = document.querySelector('.header');
-    this.nav = this.header.querySelector('.header__nav');
+    this._drawerOpen = false;
+    this._$header = $(`#${IDS.HEADER}`);
+    this._$navToggleBtn = $(`#${IDS.NAV_TOGGLER}`);
     this._$submenuTogglers = $(`.${CLASSES.SUBMENU_TOGGLER}`);
     this._$lastSubMenuDrawer = null;
 
     this._init();
   }
 
-  _init = () => {
+  _init() {
+    this._initSubmenuTogglers();
+    this._initNavToggleBtn();
+  }
+
+  _initNavToggleBtn() {
+    if (this._$navToggleBtn) {
+      this._$navToggleBtn.on('click', () => {
+        this._$header.toggleClass(CLASSES.DRAWER_OPEN);
+        this._drawerOpen = !this._drawerOpen;
+
+        // lock body scroll
+        $('body').css('overflow-y', this._drawerOpen ? 'hidden' : 'auto');
+      });
+    }
+  }
+
+  _initSubmenuTogglers() {
     if (this._$submenuTogglers) {
       this._$submenuTogglers.each((i, toggler) => {
         const $toggler = $(toggler);
@@ -23,29 +46,29 @@ export default class Header {
         // add click event to show/hide submenu
         // this is to support bare minimum keyboard accessibility
         $toggler.on('click', () => {
-          $submenu.attr('aria-hidden', $submenu.attr('aria-hidden') === 'true' ? 'false' : 'true');
+          this._showSubMenu($submenu, $submenu.attr('aria-hidden') === 'true' ? 'false' : 'true', $toggler);
         });
 
         $toggler.on('mouseenter', () => {
-          this._showSubMenu($submenu, 'false');
+          this._showSubMenu($submenu, 'false', $toggler);
         });
 
         $toggler.on('mouseleave', () => {
-          this._showSubMenu($submenu, 'true');
+          this._showSubMenu($submenu, 'true', $toggler);
         });
 
         $submenu.on('mouseenter', () => {
-          this._showSubMenu($submenu, 'false');
+          this._showSubMenu($submenu, 'false', $toggler);
         });
 
         $submenu.on('mouseleave', () => {
-          this._showSubMenu($submenu, 'true');
+          this._showSubMenu($submenu, 'true', $toggler);
         });
       });
     }
   }
 
-  _showSubMenu = ($elem, hidden) => {
+  _showSubMenu($elem, hidden, $toggler) {
     if (this._timeout) {
       clearTimeout(this._timeout);
     }
@@ -56,6 +79,7 @@ export default class Header {
       }
 
       $elem.attr('aria-hidden', hidden);
+      $toggler.attr('aria-expanded', hidden === 'false');
 
       if (hidden === 'false') {
         this._$lastSubMenuDrawer = $elem;
