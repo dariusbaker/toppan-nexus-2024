0a9ccf123e1f8a44550879103d8c2c26a57b976a
Darius Baker <hello@dariusbaker.com>
2024-04-24 16:14:19 +0800

    feat(animated-header): add and implement component

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,33 @@ import './polyfill/ie';
 // Initializations
 import './cores/prototype';
 
-$(function(){
-  console.log('Hello world.');
+const DEFAULT_FADE_UP_ANIMATION = {
+  duration: .75,
+  ease: 'expo.out',
+  opacity: 0,
+  stagger: .25,
+  y: 50,
+};
+
+function createScrollTrigger(target, timeline) {
+  ScrollTrigger.create({
+    trigger: target,
+    start: 'top 70%',
+    onEnter: () => timeline.play(),
+  });
+}
+
+$(function() {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollToPlugin);
+
+  $('[animate-stagger]').each(function () {
+    const $this = $(this);
+    const $items = $this.find('[animate-fade-up]');
+    let tl = gsap.timeline({ paused: true });
+
+    tl.from($items, DEFAULT_FADE_UP_ANIMATION);
+
+    createScrollTrigger($this, tl);
+  });
 });
